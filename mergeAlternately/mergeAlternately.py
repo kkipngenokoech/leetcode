da693da86ab26b6c19d1d1113d116f78db892a85
kip <kkipngenokoech22@gmail.com>
2023-05-22 03:45:51 +0300

    merge strings alternately

--- mergeAlternately/mergeAlternately.py
+++ mergeAlternately/mergeAlternately.py
@@ -1,15 +1,19 @@
-print("Hello multiverse")
-word1 = "Hello multiverse"
-word2 = "Hello World"
-arrayWord1 = list(word1)
-arrayWord2 = list(word2)
-mergeAlternately = ""
-while len(arrayWord1) or len(arrayWord2):
-    if len(arrayWord1):
-        char = arrayWord1.pop(0)
-        mergeAlternately += char
-    if len(arrayWord2):
-        character = arrayWord2.pop(0)
-        mergeAlternately += character
-
-print(mergeAlternately)
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        arrayWord1 = list(word1)
+        arrayWord2 = list(word2)
+        mergeAlternately = ""
+        while len(arrayWord1) or len(arrayWord2):
+            if len(arrayWord1):
+                char = arrayWord1.pop(0)
+                mergeAlternately += char
+            if len(arrayWord2):
+                character = arrayWord2.pop(0)
+                mergeAlternately += character
+        return mergeAlternately
+    
